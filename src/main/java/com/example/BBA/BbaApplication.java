11ee96a22e414eaa772f6e493500c0f2b13af6fb
Mahalakshmi M <mahalakshmi0620m@gmail.com>
2026-04-16 11:29:15 +0530

    Backend setup

---
+++ src/main/java/com/example/BBA/BbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.BBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BBA/BbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
